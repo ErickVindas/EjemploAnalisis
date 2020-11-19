89efc5f76056e5e1266c67cdfed5f3296bed119c
Jahel <jahelj29@hotmail.com>
2020-11-18 20:17:28 -0600

    Resta

--- src/ejemploanalisis/EjemploAnalisis.java
+++ src/ejemploanalisis/EjemploAnalisis.java
@@ -13,10 +13,5 @@ public class EjemploAnalisis {
     public static void main(String[] args) {
         
     }
-    public static int suma(int num1, int num2){
-        return num1 + num2;
-    }
-     public static int resta(int num1, int num2){
-        return num1 - num2;
-    }
+
 }
